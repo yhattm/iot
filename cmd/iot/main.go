a0b21bdae28dec16a4c0bedec4d3c1ab543bc24a
Ben.CP.Chen <ben@vivotek.com>
2024-01-13 23:55:26 +0000

    add gpio cmd

--- cmd/iot/main.go
+++ cmd/iot/main.go
@@ -1,9 +1,34 @@
 package main
 
 import (
-	"log"
+	"context"
+
+	"github.com/gookit/goutil/cflag"
+	"github.com/gookit/goutil/cliutil"
+	"github.com/gookit/goutil/dump"
 )
 
+var optsGpio = struct {
+	pin int
+}{}
+
 func main() {
-	log.Println("test")
+	_ = context.Background()
+	app := cflag.NewApp()
+	app.Desc = "iot"
+	app.Version = "7.1.14"
+
+	gpioCmd := cflag.NewCmd("gpio", "gpio cmd")
+	gpioCmd.OnAdd = func(c *cflag.Cmd) {
+		c.IntVar(&optsGpio.pin, "pin", 10, "gpio pin;true;t")
+	}
+	gpioCmd.Func = func(c *cflag.Cmd) error {
+		dump.P(optsGpio, c.Args())
+		cliutil.Magentaln("gpio cmd:")
+		cliutil.Infoln("gpio pin:", optsGpio.pin)
+		return nil
+	}
+	app.Add(gpioCmd)
+
+	app.Run()
 }
