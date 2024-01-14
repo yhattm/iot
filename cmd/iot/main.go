d0f7a919da6a570d07e09cd3199943bee79666d2
Ben.CP.Chen <ben@vivotek.com>
2024-01-14 00:16:06 +0000

    implement gpio

--- cmd/iot/main.go
+++ cmd/iot/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"iot/internal/gpio"
 
 	"github.com/gookit/goutil/cflag"
 	"github.com/gookit/goutil/cliutil"
@@ -10,6 +11,7 @@ import (
 
 var optsGpio = struct {
 	pin int
+	action string
 }{}
 
 func main() {
@@ -18,14 +20,22 @@ func main() {
 	app.Desc = "iot"
 	app.Version = "7.1.14"
 
+	err := gpio.Init()
+	if err == nil {
+		defer gpio.Release()
+	}
+
 	gpioCmd := cflag.NewCmd("gpio", "gpio cmd")
 	gpioCmd.OnAdd = func(c *cflag.Cmd) {
 		c.IntVar(&optsGpio.pin, "pin", 10, "gpio pin;true;t")
+		c.StringVar(&optsGpio.action, "action", "toggle", "gpio action;true;a")
 	}
 	gpioCmd.Func = func(c *cflag.Cmd) error {
 		dump.P(optsGpio, c.Args())
 		cliutil.Magentaln("gpio cmd:")
 		cliutil.Infoln("gpio pin:", optsGpio.pin)
+		action := gpio.Action(optsGpio.action)
+		gpio.SetAction(optsGpio.pin, action)
 		return nil
 	}
 	app.Add(gpioCmd)
--- internal/gpio/gpio.go
+++ internal/gpio/gpio.go
@@ -2,10 +2,32 @@ package gpio
 
 import "github.com/stianeikeland/go-rpio/v4"
 
+type Action string
+
+// define gpio action
+const (
+	ActionHigh   = "high"
+	ActionLow    = "low"
+	ActionToggle = "toggle"
+)
+
 func Init() error {
 	return rpio.Open()
 }
 
 func Release() error {
 	return rpio.Close()
+}
+
+func SetAction(pin int, action Action ) {
+	upin := uint8(pin)
+	rpio.Pin(pin).Output()
+	switch action {
+	case ActionHigh:
+		rpio.Pin(upin).High()
+	case ActionLow:
+		rpio.Pin(upin).Low()
+	case ActionToggle:
+		rpio.Pin(upin).Toggle()
+	}
 }
