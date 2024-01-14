7e2a35d7c7295a504c6ad7a3f0e080c6e2270e8b
Ben.CP.Chen <ben@vivotek.com>
2024-01-14 00:23:02 +0000

    add doc

--- internal/gpio/gpio.go
+++ internal/gpio/gpio.go
@@ -19,6 +19,7 @@ func Release() error {
 	return rpio.Close()
 }
 
+//https://datasheets.raspberrypi.com/rpi3/raspberry-pi-3-b-plus-reduced-schematics.pdf
 func SetAction(pin int, action Action ) {
 	upin := uint8(pin)
 	rpio.Pin(pin).Output()
